a5f9e0e10bc7f78969c2fbaad8471f2dddd527b8
Gabriele Nannotti <gl4d10n@gmail.com>
2024-01-28 17:13:24 +0100

    polishing

--- code/Screen_ShotSelectionForm.js
+++ code/Screen_ShotSelectionForm.js
@@ -119,9 +119,7 @@ export default function Screen_ShotSelectionForm({navigation, route}){
 
                             <ScrollView contentContainerStyle={{top: 10, width: '94%'}} >
 
-                                <Component_Header2_Bar text={image_data[i].image_description + "\n " +
-                                "Lorem ipsum dolor sit amet, consectetur adipisci elit, sed do eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullamco laboriosam, nisi ut aliquid ex ea commodi consequatur. Duis aute irure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." +
-                                "Lorem ipsum dolor sit amet, consectetur adipisci elit, sed do eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullamco laboriosam, nisi ut aliquid ex ea commodi consequatur. Duis aute irure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}/> 
+                                <Component_Header2_Bar text={image_data[i].image_description}/> 
 
                             </ScrollView>
 
